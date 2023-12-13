2b6d6c6a5a395c3df09bd6104a90dbad71c8a174
Polchik <pyrina200052004@mail.ru>
2023-12-13 17:11:32 +1000

    Добавьте файлы проекта.

---
+++ SequenceProcessing_4.5/Program.cs
@@ -0,0 +1,105 @@
+﻿//задача 1
+//int.TryParse(Console.ReadLine(), out int n);
+//double summa = 0;
+
+//for(int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    summa += num;
+//}
+//Console.WriteLine(summa);
+
+//задача 3
+//int.TryParse(Console.ReadLine(), out int n);
+//int max = int.MinValue;
+//int predMax = max;
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    if (num > max)
+//    {
+//        predMax = max;
+//        max = num;
+//    }
+
+//    else if(num > predMax)
+//        predMax = num;  
+
+//}
+//Console.WriteLine(predMax);
+
+//задача 4
+//int.TryParse(Console.ReadLine(), out int n);
+//int.TryParse(Console.ReadLine(), out int n1);
+
+//double summa = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    if(num == n1)
+//    summa++;
+//}
+//Console.WriteLine(summa);
+
+//задача 6
+//int.TryParse(Console.ReadLine(), out int n);
+//double summa = 1;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    summa *= num;
+//}
+//Console.WriteLine(summa);
+
+//задача 7
+//int.TryParse(Console.ReadLine(), out int n);
+//double summa = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    double formula = Math.Abs(num);
+//    summa += formula;
+//}
+//Console.WriteLine(summa);
+
+//задача 9
+//int.TryParse(Console.ReadLine(), out int n);
+//double summa = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    double formula = Math.Pow(num, 2);
+//    summa += formula;
+//}
+
+//Console.WriteLine(summa);
+
+//задача 10
+//int.TryParse(Console.ReadLine(), out int n);
+//double summa = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    summa += num;
+//}
+//Console.WriteLine(summa / n);
+
+//задача 11
+//int.TryParse(Console.ReadLine(), out int n);
+//double summa = 1;
+//double result = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    int.TryParse(Console.ReadLine(), out int num);
+//    summa *= num;
+
+//}
+//result = Math.Pow(summa, 1 / (double)n);
+//Console.WriteLine(result);
+
